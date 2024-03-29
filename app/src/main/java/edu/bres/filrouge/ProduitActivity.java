81e776a93cebbd2697138778bfc13c3cb63f5ca2
Bres <brestho@gmail.com>
2024-03-29 10:41:43 +0100

    correction affichage

--- app/src/main/java/edu/bres/filrouge/ProduitActivity.java
+++ app/src/main/java/edu/bres/filrouge/ProduitActivity.java
@@ -6,24 +6,28 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 public class ProduitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character);
-        Produit character = getIntent().getParcelableExtra("character");
+        Produit product = getIntent().getParcelableExtra("character");
 
         TextView titleTextView = findViewById(R.id.title);
         //Button button = findViewById(R.id.button);
         TextView characterNameText = findViewById(R.id.characterName);
-        ImageView characterPictureImage = findViewById(R.id.characterPicture);
+        ImageView picture = findViewById(R.id.productPicture);
         TextView characterDescriptionText = findViewById(R.id.characterDescription);
 
-        titleTextView.setText("OnePiece characters");
-        characterNameText.setText(character.getName());
-        //characterPictureImage.setImageResource(character.getPicture());
-        characterDescriptionText.setText(character.getDescription());
+        titleTextView.setText("Product page");
+        characterNameText.setText(product.getName());
+        Picasso.get().load(product.getPicture()).into(picture);
+        characterDescriptionText.setText(product.getDescription());
 
 
     }
